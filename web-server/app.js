b8ac63476f5ff2f5e4f9f57c0027337853058451
The Bao Dev <thebao.dev@gmail.com>
2018-11-01 09:28:24 +0700

    Styled frontside of card

---
+++ web-server/app.js
@@ -0,0 +1,26 @@
+var express = require("express"),
+  path = require("path"),
+  app = express();
+
+//set the port
+const port = process.env.PORT || 5000;
+
+app.set("port", port);
+
+//tell express that we want to use the www folder
+//for our static assets
+app.use("/", express.static(path.join(__dirname, "../www")));
+app.use(
+  "/projects/ivy",
+  express.static(path.join(__dirname, "../www/projects/ivyfood"))
+);
+
+app.use(
+  "/projects/natours",
+  express.static(path.join(__dirname, "../www/projects/natours"))
+);
+
+// Listen for requests
+var server = app.listen(app.get("port"), function() {
+  console.log("The server is running on http://localhost:" + app.get("port"));
+});
